30d3bf60a6379a61d3832b3a19004ece0ad6ab61
lcachay <lucia.cachayv@gmail.com>
2020-11-21 00:21:28 -0300

    wrap the creation of the elements in a function

--- src/app.js
+++ src/app.js
@@ -41,140 +41,144 @@ function pad(number, digits) {
    return Array(Math.max(digits - String(number).length + 1, 0)).join(0) + number;
 }
 
-/* Create elements and assign classes src href or attributes*/
+function newVideoContainer(){
+   /* Create elements and assign classes src href or attributes*/
 
-var article = document.createElement('article')
-article.classList.add('video-container')
+   var article = document.createElement('article')
+   article.classList.add('video-container')
 
-var linkVideo = document.createElement('a')
-linkVideo.classList.add('link-video')
-linkVideo.href = '#'
+   var linkVideo = document.createElement('a')
+   linkVideo.classList.add('link-video')
+   linkVideo.href = '#'
 
-var thumbnail = document.createElement('div')
-thumbnail.classList.add('thumbnail')
+   var thumbnail = document.createElement('div')
+   thumbnail.classList.add('thumbnail')
 
-//The majority of youtube videos aren't more than a few minutes long, so hours tend to be zero */  
-if (Math.random() > 0.1){
-   thumbnail.setAttribute(
-                           'data-hours', 
-   /* minutes */           Math.round(Math.random() * (60 - 1 ) + 1) + ':' + 
-   /* seconds */           pad(Math.round(Math.random() * (60 - 1 ) + 1),2) 
-   )
-}else{
-   thumbnail.setAttribute(
-                           'data-hours', 
-   /* hours */             Math.round(Math.random() * 4) + ':' + 
-   /* minutes */           pad(Math.round(Math.random() * (60 - 1 ) + 1),2) + ':' + 
-   /* seconds */           pad(Math.round(Math.random() * (60 - 1 ) + 1),2) 
-   )
+   //The majority of youtube videos aren't more than a few minutes long, so hours tend to be zero */  
+   if (Math.random() > 0.1){
+      thumbnail.setAttribute(
+                              'data-hours', 
+      /* minutes */           Math.round(Math.random() * (60 - 1 ) + 1) + ':' + 
+      /* seconds */           pad(Math.round(Math.random() * (60 - 1 ) + 1),2) 
+      )
+   }else{
+      thumbnail.setAttribute(
+                              'data-hours', 
+      /* hours */             Math.round(Math.random() * 4) + ':' + 
+      /* minutes */           pad(Math.round(Math.random() * (60 - 1 ) + 1),2) + ':' + 
+      /* seconds */           pad(Math.round(Math.random() * (60 - 1 ) + 1),2) 
+      )
+   }
+
+   var thumbnailIMG = document.createElement('img')
+   thumbnailIMG.classList.add('thumbnail-img')
+   thumbnailIMG.src = 'https://source.unsplash.com/featured/?film,people' + Math.random()
+
+   var videoText = document.createElement('div')
+   videoText.classList.add('video-text')
+
+   var channelImg = document.createElement('a')
+   channelImg.classList.add('channel-img')
+   channelImg.href = '#'
+
+   var channelImgImg = document.createElement('img')
+   channelImgImg.classList.add('channel-img')
+   channelImgImg.src = 'https://source.unsplash.com/featured/?people' + Math.random()
+
+   var videoTitle = document.createElement('a')
+   videoTitle.classList.add('video-title')
+   videoTitle.href = '#'
+
+   var videoDetails = document.createElement('div')
+   videoDetails.classList.add('video-details')
+
+   var channelName = document.createElement('a')
+   channelName.classList.add('channel-name')
+   channelName.href = '#'
+
+   var metadata = document.createElement('div')
+   metadata.classList.add('video-metadata')
+
+   var views = document.createElement('div')
+   views.classList.add('views')
+
+   var dataViewsNumber = document.createElement('span')
+   dataViewsNumber.setAttribute('data-number','views')
+
+   var viewsNumber = document.createElement('span')
+
+   var circle = document.createElement('div')
+   circle.classList.add('circle')
+
+   var circleI = document.createElement('i')
+   circleI.classList.add('fas')
+   circleI.classList.add('fa-circle')
+
+   var time = document.createElement('div')
+   time.classList.add('time')
+
+   var dataTimeNumber = document.createElement('span')
+   dataTimeNumber.setAttribute('data-number','time')
+
+   var typetimeNumber = document.createElement('span')
+   typetimeNumber.setAttribute('data-number','type')
+
+   var timeNumber = document.createElement('span')
+
+   /* Complete inner html */
+   viewsNumber.innerText = ' views'
+   timeNumber.innerText = ' ago'
+   channelName.innerText = Math.random() > 0.8 ? faker.company.companyName() :  faker.name.findName()
+   videoTitle.innerText = faker.random.words(Math.random() * 7)
+   var viewsData = Math.round(Math.random() * (999999))
+   dataViewsNumber.innerText = viewsData >= 1000 ? viewsData.toString().slice(0,-3) + 'K' : viewsData
+
+
+   var times = {
+   'seconds': Math.round(Math.random() * (60 - 1) + 1),
+   'minutes': Math.round(Math.random() * (60 - 1) + 1),
+   'hours': Math.round(Math.random() * (24 - 1) + 1),
+   'days': Math.round(Math.random() * (30 - 1) + 1),
+   'months': Math.round(Math.random() * (12 - 1) + 1),
+   'years': Math.round(Math.random() * (3 - 1) + 1)
+   };
+
+
+   typetimeNumber.innerText = Object.keys(times)[Math.floor(Math.random()*Object.keys(times).length)]
+
+   dataTimeNumber.innerText = times[typetimeNumber.innerText] + ' '
+
+   /* Append elements */
+
+   views.appendChild(dataViewsNumber)
+   views.appendChild(viewsNumber)
+
+   circle.appendChild(circleI)
+
+   time.appendChild(dataTimeNumber)
+   time.appendChild(typetimeNumber)
+   time.appendChild(timeNumber)
+
+
+   metadata.appendChild(views)
+   metadata.appendChild(circle)
+   metadata.appendChild(time)
+
+   channelImg.appendChild(channelImgImg)
+   videoDetails.appendChild(channelName)
+   videoDetails.appendChild(metadata)
+
+   videoText.appendChild(channelImg)
+   videoText.appendChild(videoTitle)
+   videoText.appendChild(videoDetails)
+
+   thumbnail.appendChild(thumbnailIMG)
+   linkVideo.appendChild(thumbnail)
+
+   article.appendChild(linkVideo)
+   article.appendChild(videoText)
+
+   main.appendChild(article)
+
 }
 
-var thumbnailIMG = document.createElement('img')
-thumbnailIMG.classList.add('thumbnail-img')
-thumbnailIMG.src = 'https://source.unsplash.com/featured/?film,people' + Math.random()
-
-var videoText = document.createElement('div')
-videoText.classList.add('video-text')
-
-var channelImg = document.createElement('a')
-channelImg.classList.add('channel-img')
-channelImg.href = '#'
-
-var channelImgImg = document.createElement('img')
-channelImgImg.classList.add('channel-img')
-channelImgImg.src = 'https://source.unsplash.com/featured/?people' + Math.random()
-
-var videoTitle = document.createElement('a')
-videoTitle.classList.add('video-title')
-videoTitle.href = '#'
-
-var videoDetails = document.createElement('div')
-videoDetails.classList.add('video-details')
-
-var channelName = document.createElement('a')
-channelName.classList.add('channel-name')
-channelName.href = '#'
-
-var metadata = document.createElement('div')
-metadata.classList.add('video-metadata')
-
-var views = document.createElement('div')
-views.classList.add('views')
-
-var dataViewsNumber = document.createElement('span')
-dataViewsNumber.setAttribute('data-number','views')
-
-var viewsNumber = document.createElement('span')
-
-var circle = document.createElement('div')
-circle.classList.add('circle')
-
-var circleI = document.createElement('i')
-circleI.classList.add('fas')
-circleI.classList.add('fa-circle')
-
-var time = document.createElement('div')
-time.classList.add('time')
-
-var dataTimeNumber = document.createElement('span')
-dataTimeNumber.setAttribute('data-number','time')
-
-var typetimeNumber = document.createElement('span')
-typetimeNumber.setAttribute('data-number','type')
-
-var timeNumber = document.createElement('span')
-
-/* Complete inner html */
-viewsNumber.innerText = ' views'
-timeNumber.innerText = ' ago'
-channelName.innerText = Math.random() > 0.8 ? faker.company.companyName() :  faker.name.findName()
-videoTitle.innerText = faker.random.words(Math.random() * 7)
-var viewsData = Math.round(Math.random() * (999999))
-dataViewsNumber.innerText = viewsData >= 1000 ? viewsData.toString().slice(0,-3) + 'K' : viewsData
-
-
-var times = {
-  'seconds': Math.round(Math.random() * (60 - 1) + 1),
-  'minutes': Math.round(Math.random() * (60 - 1) + 1),
-  'hours': Math.round(Math.random() * (24 - 1) + 1),
-  'days': Math.round(Math.random() * (30 - 1) + 1),
-  'months': Math.round(Math.random() * (12 - 1) + 1),
-  'years': Math.round(Math.random() * (3 - 1) + 1)
-};
-
-
-typetimeNumber.innerText = Object.keys(times)[Math.floor(Math.random()*Object.keys(times).length)]
-
-dataTimeNumber.innerText = times[typetimeNumber.innerText] + ' '
-
-/* Append elements */
-
-views.appendChild(dataViewsNumber)
-views.appendChild(viewsNumber)
-
-circle.appendChild(circleI)
-
-time.appendChild(dataTimeNumber)
-time.appendChild(typetimeNumber)
-time.appendChild(timeNumber)
-
-
-metadata.appendChild(views)
-metadata.appendChild(circle)
-metadata.appendChild(time)
-
-channelImg.appendChild(channelImgImg)
-videoDetails.appendChild(channelName)
-videoDetails.appendChild(metadata)
-
-videoText.appendChild(channelImg)
-videoText.appendChild(videoTitle)
-videoText.appendChild(videoDetails)
-
-thumbnail.appendChild(thumbnailIMG)
-linkVideo.appendChild(thumbnail)
-
-article.appendChild(linkVideo)
-article.appendChild(videoText)
-
-main.appendChild(article)
